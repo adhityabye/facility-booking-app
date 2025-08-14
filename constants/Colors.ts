bcc058624437eb224aaaa130f9c64176123e788f
adhityabye <mr.adhityabayu@gmail.com>
2025-08-14 12:35:19 +0700

    style: color config

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,17 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColor = '#007AFF';
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+	text: "#2C2C2E",
+	background: "#FFFFFF",
+	tint: tintColor,
+	icon: "#8E8E93",
+	tabIconDefault: "#8E8E93",
+	tabIconSelected: tintColor,
+	border: "#E0E0E0",
+	inputBackground: "#F2F2F7",
+	card: "#FFFFFF",
+	textSecondary: "#8E8E93", 
 };
